66f4fb9b5368a97ea16bdd8bf6500407ad928513
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-06 04:25:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,21 +14,19 @@ def long_planeteer_calls(array)
    array.all? do |val|
     if val.length < 4
       false
- 
     end 
   end  
   
   array.any? do |val| 
     if val.length > 4
       true
-  
     end 
   end 
-  
-  
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.any? do |val|
+    
 end
