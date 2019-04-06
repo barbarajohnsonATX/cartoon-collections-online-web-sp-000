02512888282a034b4c97a0c0a69c3296a33a10eb
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-06 04:11:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,10 @@ end
 
 def long_planeteer_calls(array)
   if array.length.any? { |length| length > 2} 
-    array.each do |length|
-    
+    return false
+  else 
+    true 
+  end 
 end
 
 def find_the_cheese# code an argument here
